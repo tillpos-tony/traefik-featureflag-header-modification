4e5e7d2f6e7ab6ad95e4567f8d3c63da107b0d58
tony <tran.d@oolio.com>
2023-06-21 14:36:08 +0700

    feat: implement featue flag check

--- featureflag_header_modification.go
+++ featureflag_header_modification.go
@@ -2,37 +2,121 @@
 package featureflag_header_modification
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 )
 
 // Config the plugin configuration.
 type Config struct {
+	Headers       []string
+	FliptEndpoint string
+	FlagKey       string
+	HeaderResult  string
 }
 
 // CreateConfig creates the default plugin configuration.
 func CreateConfig() *Config {
-	return &Config{}
+	return &Config{
+		Headers:       []string{"id"},
+		FliptEndpoint: "",
+		FlagKey:       "",
+	}
 }
 
 // XRequestStart a traefik plugin.
 type FeatureflagHeaderModification struct {
-	next http.Handler
-	name string
+	next          http.Handler
+	headers       []string
+	fliptEndpoint string
+	flagKey       string
+	headerResult  string
+	name          string
+}
+
+type FliptEvaluateResponse struct {
+	RequestID             string    `json:"requestId"`
+	EntityID              string    `json:"entityId"`
+	Match                 bool      `json:"match"`
+	FlagKey               string    `json:"flagKey"`
+	SegmentKey            string    `json:"segmentKey"`
+	Timestamp             time.Time `json:"timestamp"`
+	Value                 string    `json:"value"`
+	RequestDurationMillis float64   `json:"requestDurationMillis"`
+	Attachment            string    `json:"attachment"`
+	Reason                string    `json:"reason"`
 }
 
 // New created a new Demo plugin.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	if len(config.Headers) == 0 {
+		return nil, fmt.Errorf("headers cannot be empty")
+	}
+	if len(config.FlagKey) == 0 {
+		return nil, fmt.Errorf("FlagKeyss cannot be empty")
+	}
+	if len(config.FliptEndpoint) == 0 {
+		return nil, fmt.Errorf("FliptEndpoint cannot be empty")
+	}
+	if len(config.HeaderResult) == 0 {
+		return nil, fmt.Errorf("HeaderResult cannot be empty")
+	}
+
 	return &FeatureflagHeaderModification{
-		next: next,
-		name: name,
+		headers:       config.Headers,
+		fliptEndpoint: config.FliptEndpoint,
+		flagKey:       config.FlagKey,
+		headerResult:  config.HeaderResult,
+		next:          next,
+		name:          name,
 	}, nil
 }
 
-func (a *FeatureflagHeaderModification) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	// TODO: implementation
-	org := req.Header.Get("X-User-Org")
+func (config *FeatureflagHeaderModification) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	var org string
+	var orgHeaderName string
+	for i := 0; i < len(config.headers); i++ {
+		org = req.Header.Get(config.headers[i])
+		if len(org) != 0 {
+			orgHeaderName = config.headers[i]
+			break
+		}
+	}
 	os.Stdout.WriteString("traefik featuer flag header modification" + org)
-	a.next.ServeHTTP(rw, req)
+
+	payload := []byte(`{"entityId":"` + org + `","flagKey":"` + config.flagKey + `","context":{"` + orgHeaderName + `":"` + org + `"}}`)
+
+	resp, err := http.Post(config.fliptEndpoint, "application/json", bytes.NewBuffer(payload))
+	if err != nil {
+		os.Stderr.WriteString("Error when getting feature flag:" + err.Error())
+		config.next.ServeHTTP(rw, req)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		os.Stderr.WriteString("Error while reading response:" + err.Error())
+		config.next.ServeHTTP(rw, req)
+		return
+	}
+
+	var fliptEvaluateResponse FliptEvaluateResponse
+	err = json.Unmarshal(body, &fliptEvaluateResponse)
+	if err != nil {
+		os.Stderr.WriteString("Error while parsing response:" + err.Error())
+		config.next.ServeHTTP(rw, req)
+		return
+	}
+	if fliptEvaluateResponse.Match == true && fliptEvaluateResponse.Value == "true" {
+		req.Header.Set(config.headerResult, "1")
+	}
+
+	config.next.ServeHTTP(rw, req)
 }
